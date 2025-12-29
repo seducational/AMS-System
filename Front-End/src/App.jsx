738662478f458407b43537f19585c0252db97d9d
Yashodeep Joshi <96687722+Yashodeepj7@users.noreply.github.com>
2025-12-29 12:59:18 +0530

    Implement maintenance mode in App component
    Added maintenance mode to display Maintenance component.

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -3,8 +3,13 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./AuthContext";
 import MainContent from "./MainContent";
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
+import Maintenance from "./Components/Maintenance";
 const App = () => {
+    const isMaintenance = true; // update ke time true
+
+  if (isMaintenance) {
+    return <Maintenance />;
+  }
   return (
     <BrowserRouter>
       <AuthProvider>
